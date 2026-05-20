5864c41e79eba013581ef632059017f48c3d0a7c
Diego <diego13alejandro2005@gmail.com>
2026-05-20 08:11:02 -0500

    se fe

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from aplicacion import FuzzyApp
 
 # ── Parámetros de entrada ──────────────────────────────────────────────────────
-TEMPERATURA = 200    # °C   (0 – 300)
-TIEMPO      = 10     # min  (0 – 120)
-GROSOR      = 2.5    # cm   (0.0 – 6.0)
+TEMPERATURA = 125    # °C   (0 – 300)
+TIEMPO      = 90     # min  (0 – 120)
+GROSOR      = 3.25    # cm   (0.0 – 6.0)
 # ──────────────────────────────────────────────────────────────────────────────
 
 app = FuzzyApp(temp=TEMPERATURA, tiempo=TIEMPO, grosor=GROSOR)
